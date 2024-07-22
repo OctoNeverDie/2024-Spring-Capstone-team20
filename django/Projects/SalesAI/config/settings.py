c4fcc84aab8f4e7de2533311555b4c0d8b3debf7
Soyeon Joo <sildredel@naver.com>
2024-07-22 18:29:48 +0900

    add : gpt 4o mini 실행됨

--- django/Projects/SalesAI/config/settings.py
+++ django/Projects/SalesAI/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'CGPT', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- django/Projects/readme.txt
+++ django/Projects/readme.txt
@@ -1,2 +1,5 @@
 1. 배치파일 실행하여 가상 환경에 진입하기
 cmd에서 SalesAI 한 글자 치세여
+
+2. 서버 런은 아래
+python manage.py runserver
