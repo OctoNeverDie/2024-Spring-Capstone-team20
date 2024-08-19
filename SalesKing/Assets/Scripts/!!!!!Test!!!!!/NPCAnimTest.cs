595950bb61b67eeeaafaaa785501b1ff3d8db038
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-08-19 20:08:03 +0900

    Update NPCAnimTest.cs

--- SalesKing/Assets/Scripts/!!!!!Test!!!!!/NPCAnimTest.cs
+++ SalesKing/Assets/Scripts/!!!!!Test!!!!!/NPCAnimTest.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class NPCAnimTest : MonoBehaviour
 {
     NPCMove move;
+    NPCLooks looks;
 
     void Start()
     {
         move = GetComponent<NPCMove>();
+        looks = GetComponent<NPCLooks>();
     }
 
     void Update()
@@ -20,26 +22,32 @@ public class NPCAnimTest : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.W))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.SlightlyPositive);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][10];
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.Positive);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][10];
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.VeryPositive);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][10];
         }
         else if (Input.GetKeyDown(KeyCode.T))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.SlightlyNegative);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][6];
         }
         else if (Input.GetKeyDown(KeyCode.Y))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.Negative);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][6];
         }
         else if (Input.GetKeyDown(KeyCode.U))
         {
             move.PlayRandomNPCAnim(NPCDefine.AnimType.VeryNegative);
+            looks.thisMesh[NPCDefine.MeshType.Eyebrow].GetComponent<SkinnedMeshRenderer>().sharedMesh = Managers.NPC.Mesh.NPCMeshDictionary[NPCDefine.MeshType.Eyebrow][6];
         }
         else if (Input.GetKeyDown(KeyCode.I))
         {
