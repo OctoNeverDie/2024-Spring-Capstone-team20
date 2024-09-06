65d4a41e39174a8acfa091581394f8df285ed5fd
Soyeon Joo <sildredel@naver.com>
2024-09-06 14:48:39 +0900

    chore : cmd route

--- django/venvs/cmdSales.cmd
+++ django/venvs/cmdSales.cmd
@@ -1,4 +1,17 @@
+REM ++++Example
+REM @echo off
+REM (프로젝트 드라이브로 이동)
+REM (\django\venvs\SalesGpt\Scripts로 이동)
+REM activate
+
+REM ++++JSY notebook
 @echo off
 E:
 cd E:\gitThings\Wonder\2024-Spring-Capstone-team20\django\venvs\SalesGpt\Scripts
 activate
+
+REM ++++JSY desktop
+REM @echo off
+REM C:
+REM cd C:\Users\JSY\Desktop\2024-Spring-Capstone-team20\django\venvs\SalesGpt\Scripts
+REM activate
--- django/venvs/runSales.cmd
+++ django/venvs/runSales.cmd
@@ -1,3 +1,7 @@
 @echo off
 cd E:\gitThings\Wonder\2024-Spring-Capstone-team20\django\Projects\SalesAI
 python manage.py runserver
+
+REM @echo off
+REM cd C:\Users\JSY\Desktop\2024-Spring-Capstone-team20\django\Projects\SalesAI
+REM python manage.py runserver
