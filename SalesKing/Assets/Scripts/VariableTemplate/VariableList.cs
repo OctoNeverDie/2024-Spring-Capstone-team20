83cab3fe9400067089a652627a773c2fdd38ff55
hyunmin0809 <khm010809@naver.com>
2024-09-10 16:20:26 +0900

    fix: 게임 시스템에 맞게 구현

--- SalesKing/Assets/Scripts/VariableTemplate/VariableList.cs
+++ SalesKing/Assets/Scripts/VariableTemplate/VariableList.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,10 @@ public static class VariableList
 {
     public static int S_Affinity { get; set; }
     public static int S_Usefulness { get; set; }
-
     public static int S_AlphaPrice { get; set; }
+    public static int S_DefaultPrice { get; set; }
+
+
     //4 Things to send-------------------------------
     public static int S_ExpectedPrice { get; set; }
     public static int S_AffordablePrice { get; set; }
@@ -19,7 +22,9 @@ public static class VariableList
         S_Affinity = 0;
         S_Usefulness = 0;
 
-        S_ExpectedPrice = 0;
+        S_DefaultPrice = 10;
+        //S_DefaultPrice = (int)Math.Ceiling(S_DefaultPrice * 1.1); //이거는 나중에 
+        S_ExpectedPrice = S_DefaultPrice; 
         S_AlphaPrice = 0;
         S_AffordablePrice = S_ExpectedPrice+ S_AlphaPrice;
 
