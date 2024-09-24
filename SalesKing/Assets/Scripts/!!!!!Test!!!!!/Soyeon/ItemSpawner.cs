1df192e424571c56d4a061de4c8195e23a432a80
hyunmin0809 <khm010809@naver.com>
2024-09-24 16:31:38 +0900

    feat: 스크롤뷰 구현

--- SalesKing/Assets/Scripts/!!!!!Test!!!!!/Soyeon/ItemSpawner.cs
+++ SalesKing/Assets/Scripts/!!!!!Test!!!!!/Soyeon/ItemSpawner.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
     private List<ItemInfo> makeItemList;
-    public ItemSO itemSO;
+    [SerializeField] private GameObject itemPrefab;  // 인스펙터에서 아이템 프리팹을 지정
+    [SerializeField] Transform gridParent;
+    //public ItemSO itemSO;
 
     private void Start()
     {
@@ -15,11 +18,16 @@ public class ItemSpawner : MonoBehaviour
     void SpawnEntities()
     {
         makeItemList = Managers.Data.itemList;
-
+      
         foreach (var item in makeItemList)
-        { 
-            GameObject spawnedItem = Instantiate(itemSO.Obj3D[item.ObjID - 1], Vector3.zero, Quaternion.identity);
-            spawnedItem.name = item.ObjName + item.ObjID;
+        {
+            GameObject newItem = Instantiate(itemPrefab, gridParent);
+            // 아이템 이미지, 이름, 가격을 동적으로 설정
+            newItem.transform.Find("ItemName").GetComponent<TextMeshProUGUI>().text = item.ObjName;
+            newItem.transform.Find("Price/PriceText").GetComponent<TextMeshProUGUI>().text = item.defaultPrice.ToString() + "$";
+            // 필요한 경우 다른 데이터 추가
+            //GameObject spawnedItem = Instantiate(itemSO.Obj3D[item.ObjID - 1], Vector3.zero, Quaternion.identity);
+            //spawnedItem.name = item.ObjName + item.ObjID;
         }
     }
 
