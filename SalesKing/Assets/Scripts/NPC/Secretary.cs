fb4b40af138bc2387d7338357ba5f114344fad74
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-10-04 16:34:51 +0900

    Feat: secretary looks at you

---
+++ SalesKing/Assets/Scripts/NPC/Secretary.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secretary : MonoBehaviour
+{
+    public GameObject Neck;
+
+
+    void Update()
+    {
+        Neck.transform.LookAt(Managers.Player.MyPlayer.transform.position);
+        //Debug.Log(Managers.Player.MyPlayer.transform);
+    }
+}
