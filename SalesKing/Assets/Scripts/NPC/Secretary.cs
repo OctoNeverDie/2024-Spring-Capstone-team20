fc6d74fe9881d4c79090d0010347754ee4b2d1d5
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-10-10 12:13:06 +0900

    Feat: 말하는 비서

--- SalesKing/Assets/Scripts/NPC/Secretary.cs
+++ SalesKing/Assets/Scripts/NPC/Secretary.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Secretary : MonoBehaviour
 {
     public GameObject Neck;
-
+    public GameObject Panel;
 
     void Update()
     {
         Neck.transform.LookAt(Managers.Player.MyPlayer.transform.position);
+    }
+
+    public void ShowPanel()
+    {
+        Panel.SetActive(true);
+        StartCoroutine(HidePanelAfterDelay(3f)); // 3초 후에 패널 숨김
+    }
+
+    private IEnumerator HidePanelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // delay만큼 대기
+        Panel.SetActive(false); // 패널 비활성화
     }
 }
