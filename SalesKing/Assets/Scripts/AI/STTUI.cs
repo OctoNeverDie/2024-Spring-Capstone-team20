912b339e98a5c66fbe0e551fb8d856d3f63f3223
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-10-15 16:09:06 +0900

    Add: stt ui script

---
+++ SalesKing/Assets/Scripts/AI/STTUI.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class STTUI : MonoBehaviour
+{
+    STT2 stt;
+    public TMP_InputField myInputField;
+    public Slider recordingSlider;  // 슬라이더를 public으로 받아옴
+
+    private float currentRecordingTime = 0f;
+    private Coroutine recordingCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stt = GetComponent<STT2>();
+
+        recordingSlider.maxValue = stt._recordingLengthSec; // 슬라이더 최대값을 녹음 시간으로 설정
+        recordingSlider.value = stt._recordingLengthSec;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Managers.Input.CurInputMode == Define.UserInputMode.Voice)
+        {
+            Debug.Log("cur mode is " + Managers.Input.CurInputMode);
+            if (Input.GetButtonDown("STT"))
+            {
+                stt.StartRecording();
+            }
+            if (Input.GetButtonUp("STT"))
+            {
+                stt.StopRecording();
+            }
+        }
+    }
+
+    public void StartRecordingUI()
+    {
+        UpdateRecordingSlider();
+
+        currentRecordingTime = 0f;
+        recordingSlider.value = stt._recordingLengthSec;  // 슬라이더를 다시 채움
+
+        // Coroutine으로 슬라이더 업데이트 시작
+        if (recordingCoroutine != null)
+        {
+            StopCoroutine(recordingCoroutine);
+        }
+        recordingCoroutine = StartCoroutine(UpdateRecordingSlider());
+    }
+
+    public void StopRecordingUI()
+    {
+        recordingSlider.value = stt._recordingLengthSec;
+    }
+
+    private IEnumerator UpdateRecordingSlider()
+    {
+        while (Microphone.IsRecording(stt._microphoneID))
+        {
+            currentRecordingTime += Time.fixedDeltaTime;  // 시간이 지남에 따라 증가
+            recordingSlider.value = stt._recordingLengthSec - currentRecordingTime; // 슬라이더 값 감소
+
+            // 녹음 시간이 다 되면 녹음 중지
+            if (currentRecordingTime >= stt._recordingLengthSec)
+            {
+                stt.StopRecording();
+                yield break;
+            }
+
+            yield return null;  // 한 프레임 대기
+        }
+    }
+
+    public void SetSTTText(string newText)
+    {
+
+    }
+
+    public void ClearSTTText()
+    {
+
+    }
+}
