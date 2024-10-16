f7b5f2ef48e72e601275845ba854a200c0f8386e
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-10-16 19:43:17 +0900

    Fix: 3 해결
    1. 유저 InputField Enter키 누르면 사라짐...
    2. 녹음이랑, 떠나기 버튼 userSpeechBubble에 가려진다. 적당한 위치 찾기
    3. 마우스커서 감도 조절
    4. 된다면 : 맨처음 UI 패널 뜰 때 뒤에 게임 3D 화면 안 움직이게

---
+++ SalesKing/Assets/Scripts/UI/MouseSpeedUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MouseSpeedUI : MonoBehaviour
+{
+    public Slider slider;
+    public TextMeshProUGUI sliderText;
+
+    void Start()
+    {
+        slider.value = DataController.Instance.gameData.mouseSpeed;
+        sliderText.text = DataController.Instance.gameData.mouseSpeed.ToString();
+    }
+
+    public void UpdateMouseSpeed()
+    {
+        DataController.Instance.gameData.mouseSpeed = slider.value;
+        DataController.Instance.ToGameJson();
+        sliderText.text = slider.value.ToString();
+    }
+
+}
