d982c28c9735a73fa97c9ece121c9fcd20a1d6da
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-05 02:07:12 +0900

    Feat: stage select

--- SalesKing/Assets/Scripts/Managers/SceneManagers/StageSelectManager.cs
+++ SalesKing/Assets/Scripts/Managers/SceneManagers/StageSelectManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using OpenAI_API;
 
 public class StageSelectManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int curstage = 1;
+    CinemachineVirtualCamera Camera; // 1인칭 시점 가상 카메라
+    StageSelectUI UI;
+
     void Start()
     {
-        
+        Camera = FindObjectOfType<CinemachineVirtualCamera>();
+        UI = GameObject.Find("Canvas").GetComponent<StageSelectUI>();
+        Camera.Follow = UI.stagePanels[curstage-1].transform;
+        CheckArrowBtns();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchStages(bool isLeft)
     {
-        
+        if (isLeft)
+        {
+            curstage--;
+            Camera.Follow = UI.stagePanels[curstage-1].transform;
+        }
+        else
+        {
+            curstage++;
+            Camera.Follow = UI.stagePanels[curstage-1].transform;
+        }
+        CheckArrowBtns();
+    }
+
+    private void CheckArrowBtns()
+    {
+        // 첫 패널
+        if (curstage == 1)
+        {
+            UI.LeftBtn.gameObject.SetActive(false);
+        }
+        // 막 패널
+        else if (curstage == UI.stagePanels.Count)
+        {
+            UI.RightBtn.gameObject.SetActive(false);
+        }
+        else
+        {
+            UI.LeftBtn.gameObject.SetActive(true);
+            UI.RightBtn.gameObject.SetActive(true);
+        }
+    }
+
+    public void StartStage()
+    {
+        DataController.Instance.playData.curStageNum = curstage;
     }
 }
