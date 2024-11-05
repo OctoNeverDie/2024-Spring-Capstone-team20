a5c2c93597c463b3d595e477848dce47bd612a72
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-05 16:22:13 +0900

    Add: chatting & summary

--- SalesKing/Assets/Scripts/UI/CityUI/City_PopupAction.cs
+++ SalesKing/Assets/Scripts/UI/CityUI/City_PopupAction.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class City_PopupAction : MonoBehaviour
 {
+    void Start()
+    {
+        
+    }
 
     // 물건을 ~~ 파시면 됩니다 패널 ok 눌렀을 때
     public void OnClickOKStartPanel()
