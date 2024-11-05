d8af2be48a5d4ae6be13a1a007622e70018fbebb
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-05 16:49:37 +0900

    Feat: show tablet

--- SalesKing/Assets/Scripts/UI/CityUI/City_TabletUI.cs
+++ SalesKing/Assets/Scripts/UI/CityUI/City_TabletUI.cs
@@ -7,24 +7,24 @@ public class City_TabletUI : MonoBehaviour
 {
     public GameObject Tablet;
 
-    void OnClickShowTablet()
+    public void InitTablet()
+    {
+        Tablet.transform.localPosition = new Vector3(-2000, 0, 0);
+    }
+
+    public void OnClickShowTablet()
     {
         // Tablet을 -2000,0,0에서 0,0,0으로 이동
         Tablet.transform.localPosition = new Vector3(-2000, 0, 0);
         Tablet.transform.DOLocalMove(Vector3.zero, 1f); // 1초 동안 이동
     }
 
-    void OnClickHideTablet()
+    public void OnClickHideTablet()
     {
         // Tablet을 0,0,0에서 -2000,0,0으로 이동
+        Tablet.transform.localPosition = new Vector3(0, 0, 0);
         Tablet.transform.DOLocalMove(new Vector3(-2000, 0, 0), 1f); // 1초 동안 이동
     }
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Tab"))
-        {
-            OnClickShowTablet();
-        }
-    }
+
 }
