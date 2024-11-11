26ae06302987802483e7ac69cde66816300c3f2c
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-11 17:31:09 +0900

    Add: save file panel

--- SalesKing/Assets/Scripts/Managers/OtherSceneManagers/SaveFileManager.cs
+++ SalesKing/Assets/Scripts/Managers/OtherSceneManagers/SaveFileManager.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class SaveFileManager : MonoBehaviour
 {
     SaveFileUI UI;
+    int save_file_count = 5;
 
-    void Start()
+    private void Start()
     {
-            
+        UI = GameObject.Find("Canvas").GetComponent<SaveFileUI>();
+        SpawnSaveFilePanels();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnSaveFilePanels()
     {
-        
+        for (int i = 0; i < save_file_count; i++)
+        {
+            GameObject panel = Instantiate(UI.SaveFilePanelPrefab); // 프리팹 인스턴스화
+            panel.transform.SetParent(UI.VerticalLayoutPanel.transform, false); // VerticalLayoutPanel을 부모로 설정
+
+        }
+        /*
+        // 버튼 클릭 이벤트 연결
+        LeftBtn.onClick.AddListener(OnClickLeftBtn);
+        RightBtn.onClick.AddListener(OnClickRightBtn);
+        StartBtn.onClick.AddListener(OnClickStartBtn);
+        */
     }
 }
--- SalesKing/Assets/Scripts/UI/OtherUI/SaveFileUI.cs
+++ SalesKing/Assets/Scripts/UI/OtherUI/SaveFileUI.cs
@@ -6,30 +6,12 @@ using UnityEngine.UI;
 public class SaveFileUI : MonoBehaviour
 {
     public GameObject SaveFilePanelPrefab;
-    public GameObject Panel;
+    public GameObject VerticalLayoutPanel;
     public List<GameObject> SaveFiles = new List<GameObject>();
     public List<Button> SaveFileButtons = new List<Button>();
     public List<Button> DeleteButtons = new List<Button>();
 
-    //test
-    private void Start()
-    {
-        SpawnSaveFilePanels();        
-    }
 
-    public void SpawnSaveFilePanels()
-    {
-        for (int i = 0; i < SaveFileButtons.Count; i++)
-        {
-
-        }
-        /*
-        // 버튼 클릭 이벤트 연결
-        LeftBtn.onClick.AddListener(OnClickLeftBtn);
-        RightBtn.onClick.AddListener(OnClickRightBtn);
-        StartBtn.onClick.AddListener(OnClickStartBtn);
-        */
-    }
 
     private void OnClickSaveFileButton(int index)
     {
