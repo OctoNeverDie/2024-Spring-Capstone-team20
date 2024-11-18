a2cf08631bbfe8e59907826c2d1376963fade203
dubidubob <2076397@ewhain.net>
2024-11-19 01:07:39 +0900

    feat : input keyboard

--- SalesKing/Assets/Scripts/UI/CityUI/UserInputModeController.cs
+++ SalesKing/Assets/Scripts/UI/CityUI/UserInputModeController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UserInputMode : MonoBehaviour
 {
@@ -11,4 +13,38 @@ public class UserInputMode : MonoBehaviour
     //서버에 전송
     //도착 후 npc reply 나옴
     //1초 뒤 슬라이더로 바뀜
+    [SerializeField] private TMP_InputField userInput; 
+    [SerializeField] private Button EnterBtn;       
+
+    private void Awake()
+    {
+        ReplySubManager.OnReplyUpdated += DeleteInput;
+
+        EnterBtn.onClick.AddListener(UpdateInput);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("submit"))
+        {
+            UpdateInput();
+        }
+    }
+
+    private void UpdateInput()
+    {
+        // Ensure the input field is not empty
+        if (!string.IsNullOrEmpty(userInput.text))
+        {
+            string reply = userInput.text.Trim(); // Get and trim the input text
+            ChatManager.Instance.Reply.UserAnswer = reply; // Assign to ChatManager
+        }
+    }
+
+    private void DeleteInput(string type, string _)//응답 오면 지워짐
+    {
+        if(type == nameof(ReplySubManager.GptAnswer))
+            userInput.text = string.Empty;
+    }
 }
+
