5c759eeae8adeb21f24de86f604f0cd52d7f0b6c
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-19 16:59:29 +0900

    Feat: save file 기능

--- SalesKing/Assets/Scripts/UI/OtherUI/SaveFileMainUI.cs
+++ SalesKing/Assets/Scripts/UI/OtherUI/SaveFileMainUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SaveFileUI : MonoBehaviour
+public class SaveFileMainUI : MonoBehaviour
 {
     public GameObject SaveFilePanelPrefab;
     public GameObject VerticalLayoutPanel;
