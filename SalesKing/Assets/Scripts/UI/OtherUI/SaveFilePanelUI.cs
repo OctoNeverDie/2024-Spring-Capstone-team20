192ee1fc3656176f8f79b08d0f3330bbdbef045a
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-19 17:22:17 +0900

    Fix: conflict errors

--- SalesKing/Assets/Scripts/UI/OtherUI/SaveFilePanelUI.cs
+++ SalesKing/Assets/Scripts/UI/OtherUI/SaveFilePanelUI.cs
@@ -60,6 +60,7 @@ public class SaveFilePanelUI : MonoBehaviour
         if(!isCreateFile && !isNullPanel)
         {
             // 이 패널이 들고 있는 file ID
+            SaveFileManager.Instance.DeleteSaveFile(fileID);
         }
 
     }
