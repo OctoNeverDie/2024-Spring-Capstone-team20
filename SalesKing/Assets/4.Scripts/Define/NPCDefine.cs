981c3c4e7cbb24588db22bfa868d6e03c384136a
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-25 23:31:53 +0900

    Update NPCDefine.cs

--- SalesKing/Assets/4.Scripts/Define/NPCDefine.cs
+++ SalesKing/Assets/4.Scripts/Define/NPCDefine.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class NPCDefine : MonoBehaviour
 {
@@ -33,4 +38,24 @@ public class NPCDefine : MonoBehaviour
         Moving,
         Standing
     }
+
+    public enum EyebrowType { Aggressive, Normal, Timid }
+    public enum SexType { Male, Female, Unknown }
+    public enum AgeType { Young, Common, Old }
+    public enum BackpackType { None, Common, Wings, Tube, Youtuber, Jetpack, Hiking, Sword, Gun }
+    public enum FullbodyType { None, OuterSpace, Soldier, Halloween, Food, Party }
+    public enum GlassesType { None, CommonGlasses, Sunglasses, Special }
+    public enum GloveType { None, Common, Boxing, Ski, LadyGloves, WorkGloves }
+    public enum HairType { None, MaleCommon, FemaleCommon, Oriental, Workout }
+    public enum HatType
+    {
+        None, Common, Cap, Headphones, Beret, Theif, Police, Farmer, Chef, Maid, Firefighter, Goggles, Graduate,
+        ConstructionWorker, Pilot, SerialKiller, Clown, CuteCostume, Pancho, PowderedWig, Elf, Kid, Santa, Gentleman,
+        Helmet, Cleopatra, Snorkling, Lady, Crown, HeroMask, WinterHat, Yoda, GasMask, JackSparrow, PaperBag, Alien, PlagueMask
+    }
+    public enum MustacheType { None, Common, Thin, Beard, White }
+    public enum OuterwearType { Hoodie, TankTop, Dress, Shirt, Jacket, Suit, TShirt }
+    public enum PantsType { None, Shorts, Pants, Skirt }
+    public enum ShoeType { None, Sneakers, Boots, Skate, Slippers }
+
 }
