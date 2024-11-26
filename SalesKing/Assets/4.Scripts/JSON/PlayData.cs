5770dc041883a4035d7d3d24bfcbb9f240081432
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-11-26 19:31:20 +0900

    Add: end day ui

--- SalesKing/Assets/4.Scripts/JSON/PlayData.cs
+++ SalesKing/Assets/4.Scripts/JSON/PlayData.cs
@@ -9,7 +9,18 @@ using System;
 public class PlayData
 {
     // 지금 현재 플레이하고 있는 날짜
-    public int cur_day_ID = 0;
+    private int _cur_day_ID; // 실제 값을 저장하는 필드
+
+    public int cur_day_ID
+    {
+        get => _cur_day_ID; // 값을 반환할 때는 백킹 필드 사용
+        set
+        {
+            Debug.Log($"Day changed from {_cur_day_ID} to {value}");
+            _cur_day_ID = value; // 값을 백킹 필드에 저장
+        }
+    }
+
     // 지금까지 플레이했던 날들의 데이터 리스트 
     public List<DayData> item_list = new List<DayData>();
 }
