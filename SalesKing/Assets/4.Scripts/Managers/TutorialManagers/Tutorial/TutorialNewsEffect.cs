925c5b95258a1a3d8461a0f60afd2a3b1c1ce7f2
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2024-12-28 11:02:01 +0900

    Add: effects

--- SalesKing/Assets/4.Scripts/Managers/TutorialManagers/Tutorial/TutorialNewsEffect.cs
+++ SalesKing/Assets/4.Scripts/Managers/TutorialManagers/Tutorial/TutorialNewsEffect.cs
@@ -5,56 +5,64 @@ using DG.Tweening;
 
 public class TutorialNewsEffect : TutorialBase
 {
+    [SerializeField] private GameObject news_object;
     [SerializeField] private List<GameObject> news_list;
+    private AudioSource source;
+    private AudioClip sfx;
+
     private float big_scale = 3f;
-    private float duration = 2f;
+    private float duration = 0.5f;
+    private bool isEffectOver = false;
 
     public override void Enter()
     {
+        isEffectOver = false;
+        source = news_object.GetComponent<AudioSource>();
+
         for (int i = 0; i < news_list.Count; i++)
         {
             news_list[i].transform.localScale = new Vector3(0, 0, 0);
         }
-    }
+        
 
-    public override void Execute(TutorialController controller)
-    {
         Sequence sequence = DOTween.Sequence();
 
         for (int i = 0; i < news_list.Count; i++)
         {
-            sequence.Append(news_list[i].transform.DOScale(new Vector3(big_scale, big_scale, big_scale), 0.1f)); 
-            sequence.Append(news_list[i].transform.DOScale(new Vector3(1, 1, 1), duration));
-        }
-        /*
-        sequence.AppendCallback(() => {
-
-            Sequence moveSequence = DOTween.Sequence();
-
-            for (int i = 0; i < news_list.Count; i++)
+            sequence.Append(news_list[i].transform.DOScale(new Vector3(big_scale, big_scale, big_scale), 0.01f).SetEase(Ease.OutBounce));
+            sequence.Append(news_list[i].transform.DOScale(new Vector3(1, 1, 1), duration).SetEase(Ease.InOutQuad));
+            sequence.AppendCallback(() =>
             {
-                
-                moveSequence.Append(transform.DOMoveY(transform.position.y + moveDistance, moveDuration)); // 위로 이동
-                moveSequence.Append(transform.DOFade(0, moveDuration)); // 점차적으로 사라짐
-                
-            }
+                source.Play();
+            });
+        }
 
-            moveSequence.Play(); // 이동 애니메이션 실행
+        sequence.AppendInterval(1f);
+
+        sequence.AppendCallback(() => {
+             news_object.transform.DOMoveY(news_object.transform.position.y + 1500f, 2f);
         });
-        */
+        
         sequence.OnComplete(() =>
         {
-            controller.SetNextTutorial();
+            isEffectOver = true;
         });
 
         sequence.Play();
     }
 
+    public override void Execute(TutorialController controller)
+    {
+        if(isEffectOver) controller.SetNextTutorial();
+    }
+
     public override void Exit()
     {
+        /*
         for (int i = 0; i < news_list.Count; i++)
         {
             news_list[i].transform.localScale = new Vector3(0, 0, 0);
         }
+        */
     }
 }
